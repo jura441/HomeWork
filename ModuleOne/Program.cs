dcd60cd05b9e6389e07e49ff68926b109e0fb6d5
Юрий Иванов <jura401@mail.ru>
2022-09-27 23:45:17 +0300

    ModuleOne

---
+++ ModuleOne/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+double num1, num2, num3, num4;
+int result;
+
+Console.WriteLine("Введите 1 цифру");
+num1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 цифру");
+num2 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 цифру");
+num3 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите 4 цифру");
+num4 = Double.Parse(Console.ReadLine());
+
+result = (int)num1 * 1000 + (int)num2 * 100 + (int)num3 * 10 + (int)num4;
+Console.WriteLine("Полученный результат = {0}", result);
