004d8ad0b3778fee59ecc163499327eb72538192
Юрий Иванов <jura401@mail.ru>
2022-10-12 20:34:08 +0300

    BuildingaHouse

---
+++ Building a house/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_a_house
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Teams> teams = new List<Teams>
+                { new Teams("Рабочий 1"),
+                new Teams("Рабочий 2"),
+                new Teams("Рабочий 3"),
+                new Teams("Рабочий 4") };
+            House house = new House();
+            Team teamleader = new Team("Бригадир", teams);
+            //докладная бригадира по строительству дома
+            Console.WriteLine(teamleader.Name);
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                teamleader.Teams[rand.Next(0, 3)].Build(house, teamleader.T);
+            }
+
+            foreach (var j in teamleader.T.report)
+            {
+                Console.WriteLine(j);
+            }
+
+            teamleader.T.Report();
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                teamleader.Teams[rand.Next(0, 3)].Build(house, teamleader.T);
+            }
+
+            foreach (var i in teamleader.T.report)
+            {
+                Console.WriteLine(i);
+            }
+            teamleader.T.Report();
+
+            house.Build(teamleader.T);
+        }
+    }
+    interface IWorker
+    {
+        string Name { get; }
+    }
+    interface IPart
+    {
+        void Build(House house);
+    }
+
+    class Basement : IPart
+    {
+        public void Build(House house)
+        {
+            house.basement = new Basement();
+        }
+    }
+
+    class Walls : IPart
+    {
+        public void Build(House house)
+        {
+            house.walls.Add(new Walls());
+        }
+    }
+
+    class Door : IPart
+    {
+        public void Build(House house)
+        {
+            house.door = new Door();
+        }
+    }
+
+    class Window : IPart
+    {
+        public void Build(House house)
+        {
+            house.window.Add(new Window());
+        }
+    }
+
+    class Roof : IPart
+    {
+        public void Build(House house)
+        {
+            house.roof = new Roof();
+        }
+    }
+
+    class House
+    {
+        public Basement basement;
+        public List<Walls> walls;
+        public List<Window> window;
+        public Door door;
+        public Roof roof;
+        public void Build(TeamLeader t)
+        {
+            Console.WriteLine("\n___________$$$\r\n_________$$___$$\r\n_____$$$_______$$$\r\n__$$$_____________$$$\r\n__-------------------\r\n__$$$$$$$$$$$$$$$$$$$\r\n__$_________________$\r\n__$____$$$$$$$$$____$\r\n__$____$        $___$\r\n__$____$        $___$\r\n__$____$$$$$$$$$____$\r\n__$_________________$\r\n__$$$$$$$$$$$$$$$$$$$\r\n__-------------------\r\n");
+
+        }
+    }
+    class Team : IWorker
+    {
+        public TeamLeader T;
+        public List<Teams> Teams;
+        public string Name { get; set; }
+
+        public Team(string name, List<Teams> workers)
+        {
+            T = new TeamLeader(name);
+            Teams = workers;
+        }
+    }
+
+    class Teams : IWorker
+    {
+        string Name { get; set; }
+
+        string IWorker.Name => Name;
+
+        public Teams(string name)
+        { Name = name; }
+
+        public void Build(House house, TeamLeader T)
+        {
+            if (house.basement == null)
+            {
+                Basement basement = new Basement();
+                basement.Build(house);
+                T.report.Add($"{Name} залил фундамент");
+            }
+            else if (house.walls == null || house.walls.Count < 4)
+            {
+                if (house.walls == null) house.walls = new List<Walls>();
+                Walls wall = new Walls();
+                wall.Build(house);
+                T.report.Add($"{Name} построил стену {house.walls.Count}");
+            }
+            else if (house.roof == null)
+            {
+                Roof roof = new Roof();
+                roof.Build(house);
+                T.report.Add($"{Name} построил крышу");
+            }
+            else if (house.door == null)
+            {
+                Door door = new Door();
+                door.Build(house);
+                T.report.Add($"{Name} поставил дверь на запасной выход с обратной стoроны дома");
+            }
+            else if (house.window == null || house.window.Count < 4)
+            {
+                if (house.window == null) house.window = new List<Window>();
+                Window window = new Window();
+                window.Build(house);
+                T.report.Add($"{Name} установил окно {house.window.Count}");
+            }
+        }
+    }
+    class TeamLeader : IWorker
+    {
+        string Name { get; set; }
+        public List<string> report = new List<string>();
+        string IWorker.Name => Name;
+        public TeamLeader(string name)
+        { Name = name; }
+        public void Report()
+        {
+
+            Console.WriteLine($"{Name} сделал отчёт, что строительство закончено");
+        }
+    }
+}
--- ModuleFive/Program.cs
+++ ModuleFive/Program.cs
@@ -5,174 +5,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModuleFive
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Book> list = new List<Book>();
-            list.Add(new Book("Оно", "America", "Horror", "Stephen King", 700, 1995, 1000));
-            list.Add(new Book("Оно2", "America", "Horror", "Stephen King", 700, 1995, 1000));
-            list.Add(new Book("Оно3", "America", "Horror", "Stephen King", 700, 1995, 1000));
+//namespace ModuleFive
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<Book> list = new List<Book>();
+//            list.Add(new Book("Оно", "America", "Horror", "Stephen King", 700, 1995, 1000));
+//            list.Add(new Book("Оно2", "America", "Horror", "Stephen King", 700, 1995, 1000));
+//            list.Add(new Book("Оно3", "America", "Horror", "Stephen King", 700, 1995, 1000));
 
-            Shop sh = new Shop(Resources.ShopName, "ТЦ \"Рассвет\"", list);
-            sh.GetShopName();
-            Command.HelpCommand();
-            while (true)
-            {
-                Console.WriteLine("Введите новую команду: ");
-                string command = Console.ReadLine();
-                switch (command)
-                {
-                    case "help": Command.HelpCommand(); break;
-                    case "addbook": sh.AddABook(Command.AddBookCommand()); break;
-                    case "removebook": sh.DeleteBookByIndex(Command.RemoveBookCommand()); break;
-                    case "removebookname": sh.DeleteBookByName(Command.RemoveBookCommandName()); break;
-                    case "getbooks": Command.GetAllBookCommand(); sh.GetAllBooks(); break;
-                    default: Console.WriteLine("Не удалось распознать команду. Наберите Help для списка комманд"); break;
-                }
+//            Shop sh = new Shop(Resources.ShopName, "ТЦ \"Рассвет\"", list);
+//            sh.GetShopName();
+//            Command.HelpCommand();
+//            while (true)
+//            {
+//                Console.WriteLine("Введите новую команду: ");
+//                string command = Console.ReadLine();
+//                switch (command)
+//                {
+//                    case "help": Command.HelpCommand(); break;
+//                    case "addbook": sh.AddABook(Command.AddBookCommand()); break;
+//                    case "removebook": sh.DeleteBookByIndex(Command.RemoveBookCommand()); break;
+//                    case "removebookname": sh.DeleteBookByName(Command.RemoveBookCommandName()); break;
+//                    case "getbooks": Command.GetAllBookCommand(); sh.GetAllBooks(); break;
+//                    default: Console.WriteLine("Не удалось распознать команду. Наберите Help для списка комманд"); break;
+//                }
 
 
-            }
-        }
+//            }
+//        }
 
-class Book : IEnumerable, IComparable
-        {
-            public string Name { get; set; } 
-            public int NumberPages { get; set; }
-            public string Publishing { get; set; }
-            public string Genre { get; set; }
-            public string Author { get; set; }
-            public int YearOfPublishing { get; set; }
-            public int Price { get; set; }
+//class Book : IEnumerable, IComparable
+//        {
+//            public string Name { get; set; } 
+//            public int NumberPages { get; set; }
+//            public string Publishing { get; set; }
+//            public string Genre { get; set; }
+//            public string Author { get; set; }
+//            public int YearOfPublishing { get; set; }
+//            public int Price { get; set; }
 
-            public Book(string name, string publishing, string genre, string author, int numberpages, int yearofpublishing, int price)
+//            public Book(string name, string publishing, string genre, string author, int numberpages, int yearofpublishing, int price)
 
-            {
-                Name = name;
-                Publishing = publishing;
-                Genre = genre;
-                Author = author;
-                NumberPages = numberpages;
-                YearOfPublishing = yearofpublishing;
-                Price = price;
-            }
-            public override string ToString()
-            {
-                return Name + " " + Publishing + " " + Genre + " " + Author + " " + NumberPages.ToString() + " " + YearOfPublishing.ToString() + " " + Price.ToString();
-            }
-            public IEnumerator GetEnumerator()
-            {
-                return ((IEnumerable)Name).GetEnumerator();
-            }
-            public int CompareTo(object? obj)
-            {
-                return Name.CompareTo((string?)obj);
-            }
-            public int Compare(object? obj, object? obj2)
-            {
-                return ((Book)obj).Name.CompareTo((string?)obj2);
-            }
+//            {
+//                Name = name;
+//                Publishing = publishing;
+//                Genre = genre;
+//                Author = author;
+//                NumberPages = numberpages;
+//                YearOfPublishing = yearofpublishing;
+//                Price = price;
+//            }
+//            public override string ToString()
+//            {
+//                return Name + " " + Publishing + " " + Genre + " " + Author + " " + NumberPages.ToString() + " " + YearOfPublishing.ToString() + " " + Price.ToString();
+//            }
+//            public IEnumerator GetEnumerator()
+//            {
+//                return ((IEnumerable)Name).GetEnumerator();
+//            }
+//            public int CompareTo(object obj)
+//            {
+//                return Name.CompareTo((string)obj);
+//            }
+//            public int Compare(object obj, object obj2)
+//            {
+//                return ((Book)obj).Name.CompareTo((string)obj2);
+//            }
 
-        }
-        class Shop
-        {
-            List<Book> Books;
+//        }
+//        class Shop
+//        {
+//            List<Book> Books;
 
-            string Name { get; set; }
+//            string Name { get; set; }
 
-            string Address { get; set; }
+//            string Address { get; set; }
 
-            public Shop(string name, string address, List<Book> books)
-            {
-                Name = name;
-                Address = address;
-                Books = books;
-            }
+//            public Shop(string name, string address, List<Book> books)
+//            {
+//                Name = name;
+//                Address = address;
+//                Books = books;
+//            }
 
-            public void GetShopName()
-            {
-                Console.WriteLine("Добро пожаловать в {0}", Name);
-            }
-            public void SetShopName()
-            {
-                Console.WriteLine();
-            }
-            public void GetAllBooks()
-            {
-                int index = 1;
-                foreach (Book book in Books)
-                {
-                    Console.WriteLine("Индекс:");
-                }
+//            public void GetShopName()
+//            {
+//                Console.WriteLine("Добро пожаловать в {0}", Name);
+//            }
+//            public void SetShopName()
+//            {
+//                Console.WriteLine();
+//            }
+//            public void GetAllBooks()
+//            {
+//                int index = 1;
+//                foreach (Book book in Books)
+//                {
+//                    Console.WriteLine("Индекс:");
+//                }
 
-            }
-            public void AddABook(Book book)
-            {
-                Books.Add(book);
-            }
-            public void DeleteBookByName(string name)
-            {
-                int index = -1;
-                foreach (Book book in Books)
-                {
+//            }
+//            public void AddABook(Book book)
+//            {
+//                Books.Add(book);
+//            }
+//            public void DeleteBookByName(string name)
+//            {
+//                int index = -1;
+//                foreach (Book book in Books)
+//                {
 
-                    if (book.CompareTo(name) == 0) index = Books.IndexOf(book);
-                }
-            }
-            public void DeleteBookByIndex(int index)
-            {
-                Books.RemoveAt(index);
-            }
+//                    if (book.CompareTo(name) == 0) index = Books.IndexOf(book);
+//                }
+//            }
+//            public void DeleteBookByIndex(int index)
+//            {
+//                Books.RemoveAt(index);
+//            }
 
-        }
+//        }
 
-        class Command
+//        class Command
 
-        {
-            public static void HelpCommand()
-            {
-                Console.WriteLine("Используйте addbook для добавления книги, removebook для удаления книги, getbook для списка книг, clear для очистки ");
-            }
-            public static Book AddBookCommand()
-            {
-                Book book;
-                Console.WriteLine("Введите имя книги");
-                string name = Console.ReadLine();
-                Console.WriteLine("Введите издательство  книги");
-                string publishing = Console.ReadLine();
-                Console.WriteLine("Введите жанр книги");
-                string genre = Console.ReadLine();
-                Console.WriteLine("Введите автор книги");
-                string author = Console.ReadLine();
-                Console.WriteLine("Введите количество страниц книги");
-                int numberpage = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Введите год издания книги");
-                int yearofpublishing = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Введите цену книги");
-                int price = Int32.Parse(Console.ReadLine());
-                book = new Book(name, publishing, genre, author, numberpage, yearofpublishing, price);
-                return book;
-            }
-            public static string RemoveBookCommandName()
-            {
-                Console.WriteLine("Укажите имя удаляемой книги:");
-                return Console.ReadLine();
-            }
-            public static int RemoveBookCommand()
-            {
-                Console.WriteLine("Укажите индекс удаляемой книги:");
-                return Int32.Parse(Console.ReadLine());
-            }
-            public static void GetAllBookCommand()
-            {
-                Console.WriteLine("Список книг в магазине: ");
-            }
+//        {
+//            public static void HelpCommand()
+//            {
+//                Console.WriteLine("Используйте addbook для добавления книги, removebook для удаления книги, getbook для списка книг, clear для очистки ");
+//            }
+//            public static Book AddBookCommand()
+//            {
+//                Book book;
+//                Console.WriteLine("Введите имя книги");
+//                string name = Console.ReadLine();
+//                Console.WriteLine("Введите издательство  книги");
+//                string publishing = Console.ReadLine();
+//                Console.WriteLine("Введите жанр книги");
+//                string genre = Console.ReadLine();
+//                Console.WriteLine("Введите автор книги");
+//                string author = Console.ReadLine();
+//                Console.WriteLine("Введите количество страниц книги");
+//                int numberpage = Int32.Parse(Console.ReadLine());
+//                Console.WriteLine("Введите год издания книги");
+//                int yearofpublishing = Int32.Parse(Console.ReadLine());
+//                Console.WriteLine("Введите цену книги");
+//                int price = Int32.Parse(Console.ReadLine());
+//                book = new Book(name, publishing, genre, author, numberpage, yearofpublishing, price);
+//                return book;
+//            }
+//            public static string RemoveBookCommandName()
+//            {
+//                Console.WriteLine("Укажите имя удаляемой книги:");
+//                return Console.ReadLine();
+//            }
+//            public static int RemoveBookCommand()
+//            {
+//                Console.WriteLine("Укажите индекс удаляемой книги:");
+//                return Int32.Parse(Console.ReadLine());
+//            }
+//            public static void GetAllBookCommand()
+//            {
+//                Console.WriteLine("Список книг в магазине: ");
+//            }
             
 
-        }
+//        }
 
-    }
+//    }
     
-}
+//}
